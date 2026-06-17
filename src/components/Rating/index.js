9b0982d68de21b7b625319b790de430089ed1e5b
limitless-v <developerforlkp@gmail.com>
2026-06-17 12:17:48 +0530

    stars

--- src/components/Rating/index.js
+++ src/components/Rating/index.js
@@ -1,11 +1,18 @@
 import React from "react";
-// import cn from "classnames";
 import styles from "./Rating.module.sass";
 import Rating from "react-rating";
 
 const Form = ({ className, initialRating, rating, onChange, readonly }) => {
   // Use controlled rating if provided, otherwise use initialRating
   const ratingValue = rating !== undefined ? rating : initialRating;
+
+  const renderStar = (starClassName) => (
+    <span className={`${styles.star} ${starClassName}`} aria-hidden="true">
+      <svg viewBox="0 0 24 24" focusable="false">
+        <path d="M12 17.27 18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
+      </svg>
+    </span>
+  );
   
   return (
     <Rating
@@ -13,20 +20,8 @@ const Form = ({ className, initialRating, rating, onChange, readonly }) => {
       initialRating={ratingValue}
       onChange={onChange}
       readonly={readonly}
-      emptySymbol={
-        <img
-          src=""
-          className={styles.star}
-          alt=""
-        />
-      }
-      fullSymbol={
-        <img
-          src=""
-          className={styles.star}
-          alt=""
-        />
-      }
+      emptySymbol={renderStar(styles.starEmpty)}
+      fullSymbol={renderStar(styles.starFull)}
     />
   );
 };
