716ab8d5903166dee6f27d5a3a0223b4928d490f
4dithyan <mailforadithyan@gmail.com>
2026-07-17 15:50:43 +0530

    place detail page done

--- src/screens/PlaceDetails/index.js
+++ src/screens/PlaceDetails/index.js
@@ -3013,13 +3013,14 @@ function MobilePlaceDetails({
       {/* 8. Booking / CTA Removed as requested */}
 
       {/* 9. Related Listings Strip */}
-      <div style={{ padding: "0 16px" }}>
+      <div>
         <RelatedListingsStrip
           businessInterestId={4}
           primaryCategoryId={primaryCategoryId}
           currentListingId={currentListingId}
           title="More Places To Explore"
-          titleStyle={{ fontSize: 24, marginBottom: 16 }}
+          sectionStyle={{ padding: "40px 16px" }}
+          titleStyle={{ fontSize: "clamp(1.8rem, 2.5vw, 2.2rem)", fontWeight: 700, marginBottom: "32px", fontFamily: "Poppins, sans-serif" }}
         />
       </div>
 
@@ -3248,6 +3249,8 @@ const PlaceDetails = () => {
         primaryCategoryId={primaryCategoryId}
         currentListingId={currentListingId}
         title="More Places To Explore"
+        sectionStyle={{ padding: "48px 80px" }}
+        titleStyle={{ fontSize: "clamp(1.8rem, 2.5vw, 2.2rem)", fontWeight: 700, marginBottom: "32px", fontFamily: "Poppins, sans-serif" }}
       />
 
       <Footer />
